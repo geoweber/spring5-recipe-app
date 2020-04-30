8746bd2b4b441e5ed83cf790ea93bd33d290e8dc
Georg Weber <geo.weber@t-online.de>
2020-05-01 00:47:55 +0200

    minor changes, logback, messages_de, swagger

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -2,8 +2,10 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
